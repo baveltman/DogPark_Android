7b13ed14ab85ce771f65d4880c48e03fdca8a85f
baveltman <baveltman@gmail.com>
2015-04-08 23:51:26 -0500

    add dog fragment layout death

--- app/src/main/java/apps/baveltman/dogpark/AddDogFragment.java
+++ app/src/main/java/apps/baveltman/dogpark/AddDogFragment.java
@@ -1,12 +1,17 @@
 package apps.baveltman.dogpark;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AddDogFragment extends Fragment {
+
+    private EditText mDogInfo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,8 @@ public class AddDogFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_add_dog, parent, false);
+
+        Typeface myTypeface = Typeface.createFromAsset(getActivity().getAssets(), "BEBAS.ttf");
 
         return v;
     }
